a27368cc1206a679e7fec0bdfe02b91a01ca3b4e
shi0n0 <shiono.yui1080@gmail.com>
2023-08-19 00:00:06 +0900

    余計な記述を変更

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -1,4 +1,6 @@
+import { error } from "console";
+
 export default async function Page() {
     await new Promise((resolve) => setTimeout(resolve, 1000));
-    return <div>this is hello pageかもしれないね!</div>
+    return <div>this is hello page!</div>
 }
