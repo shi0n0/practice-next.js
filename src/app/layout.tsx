06aca568508562135192bd0722dbf5a30b9f93c2
shi0n0 <shiono.yui1080@gmail.com>
2023-08-19 00:01:33 +0900

    <main>タグを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Noto_Sans_JP } from 'next/font/google'
 import './globals.css'
 
-// 1. フォントの読み込み
+// フォントの読み込み
 const NotoSansJP = Noto_Sans_JP({
   weight: ["400", "700"],
   subsets: ["latin"],
@@ -9,13 +9,13 @@ const NotoSansJP = Noto_Sans_JP({
 });
 
 
-// 1. ページのメタデータ
+// ページのメタデータ
 export const metadata = {
   title: 'Next.js TEST-APP',
   description: 'Test project using Next.js 13.4',
 }
 
-// 2. ページのレイアウト
+// ページのレイアウト
 export default function RootLayout({
   children,
 }: {
@@ -25,7 +25,9 @@ export default function RootLayout({
     <html lang="ja">
       <body className={NotoSansJP.className}>
           <div>this div is shared layout.tsx</div>
-          {children}
+          <main>
+            {children}
+          </main>
       </body>
     </html>
   )
