586224a9b0e4eaf36f2676a864089ba82306376f
shi0n0 <shiono.yui1080@gmail.com>
2023-08-19 22:15:54 +0900

    簡単なローディングアニメーションを追加

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,7 @@
 export default function Loading() {
     return(
-        <h1>Loading...</h1>
+        <div className="flex justify-center" aria-label="読み込み中">
+            <div className="animate-spin h-8 w-8 bg-blue-300 rounded-xl"></div>
+        </div>
     )
 }
