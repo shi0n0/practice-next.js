37761f5a6e23c7b8dd92962bb0201b5c92b74275
shi0n0 <shiono.yui1080@gmail.com>
2023-08-20 23:59:33 +0900

    submitを追加

---
+++ src/app/submit.tsx
@@ -0,0 +1,9 @@
+export default function Submit() {
+    return(
+        <div>
+            <div className="border border-solid border-black w-9/12 flex justify-center">
+
+            </div>
+        </div>
+    );
+}
