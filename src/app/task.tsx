58b8d7c489330540d4784e3d8de63a43283aa458
shi0n0 <shiono.yui1080@gmail.com>
2023-08-21 01:27:27 +0900

    タスクを表示するコンポーネント

---
+++ src/app/task.tsx
@@ -0,0 +1,7 @@
+export default function Task() {
+    return(
+        <div className="rounded-lg bg-white p-3 whitespace-normal overflow-wrap break-words">
+            テストをテストしてテスト内で問題が出たのでテストして再テストをすると治った
+        </div>
+    )
+}
