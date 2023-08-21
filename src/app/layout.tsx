785d43139015485f3f4972c6168743f93c581f55
shi0n0 <shiono.yui1080@gmail.com>
2023-08-22 02:08:04 +0900

    サイト全体に適応するCSSを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Noto_Sans_JP } from 'next/font/google'
 import './globals.css'
-import Navbar from './navbar';
+import Navbar from './components/navbar';
 
 // フォントの読み込み
 const NotoSansJP = Noto_Sans_JP({
@@ -26,9 +26,13 @@ export default function RootLayout({
     <html lang="ja">
       <body className={`${NotoSansJP.className} bg-gray-200`}>
       <Navbar />
-          <div>this div is shared layout.tsx</div>
-          <main>
-            {children}
+        <div className='text-center mt-10'>
+          <h1 className='text-3xl font-bold text-gray-700'>Next.js-13.4 Todo App</h1>
+        </div>
+          <main className='flex flex-col items-center justify-center min-h-screen -mt-32'>
+            <div className='w-full max-w-xl mt-5'>
+              {children}
+            </div>
           </main>
       </body>
     </html>
