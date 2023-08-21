c8ed7e26a78c6b3043c06fb425486d8928baa81a
shi0n0 <shiono.yui1080@gmail.com>
2023-08-22 00:01:38 +0900

    componentsフォルダーに統合されるため移動

--- src/app/submit.tsx
+++
@@ -1,9 +0,0 @@
-export default function Submit() {
-    return(
-        <div className="p-4 text-center">
-            <input type="text" className="" />
-        </div>
-
-
-    );
-}
--- src/app/task.tsx
+++
@@ -1,7 +0,0 @@
-export default function Task() {
-    return(
-        <div className="rounded-lg bg-white p-3 whitespace-normal overflow-wrap break-words">
-            テストをテストしてテスト内で問題が出たのでテストして再テストをすると治った
-        </div>
-    )
-}
