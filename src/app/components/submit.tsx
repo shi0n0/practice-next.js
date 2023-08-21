81c583da58f909549c17e90958bae470bc9d0dc2
shi0n0 <shiono.yui1080@gmail.com>
2023-08-22 02:05:57 +0900

    タスク登録のCSSを追加

--- src/app/components/submit.tsx
+++ src/app/components/submit.tsx
@@ -1,7 +1,8 @@
 export default function Submit() {
     return(
-        <div className="p-4 text-center">
-            <input type="text" className="" />
-        </div>
+        <form action="" className="flex flex-col">
+            <input type="text" className="p-2 m-2 border rounded focus:outline-none" />
+            <button className="bg-gray-200">タスク追加</button>
+        </form>
     );
 }
