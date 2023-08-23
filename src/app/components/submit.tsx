7283b133666fcbc0d8def71d1f335e09ed22f651
shi0n0 <shiono.yui1080@gmail.com>
2023-08-24 00:00:00 +0900

    ボタンのデザインを変更

--- src/app/components/submit.tsx
+++ src/app/components/submit.tsx
@@ -2,7 +2,7 @@ export default function Submit() {
     return(
         <form action="" className="flex flex-col">
             <input type="text" className="p-2 m-2 border rounded focus:outline-none" />
-            <button className="bg-gray-200">タスク追加</button>
+            <button className="w-full text-white bg-blue-500">タスク追加</button>
         </form>
     );
 }
