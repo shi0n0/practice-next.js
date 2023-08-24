cf939e08c0255c4c6132e0dc9587ede15268c343
shi0n0 <shiono.yui1080@gmail.com>
2023-08-24 23:54:33 +0900

    ボタンデザイン修正

--- src/app/components/submit.tsx
+++ src/app/components/submit.tsx
@@ -2,7 +2,7 @@ export default function Submit() {
     return(
         <form action="" className="flex flex-col">
             <input type="text" className="p-2 m-2 border rounded focus:outline-none" />
-            <button className="w-full text-white bg-blue-500">タスク追加</button>
+            <button className="text-white bg-blue-500 p-2 m-2 transform hover:bg-blue-400 hover:scale-95 duration-100">タスク追加</button>
         </form>
     );
 }
