cc6a17984f33bbe48d2136ab7611ae3065e3863d
shi0n0 <shiono.yui1080@gmail.com>
2023-08-25 00:25:46 +0900

    CSSを付与

--- src/app/components/task.tsx
+++ src/app/components/task.tsx
@@ -2,12 +2,12 @@ import React from "react"
 
 const Task = () => {
     return(
-        <ul>
-            <li>
+        <ul className="border-l-4 rounded-l p-2 border-blue-400 bg-gradient-to-r from-blue-200 shadow">
+            <li className="flex justify-between p-3">
                 <span>Next.jsの勉強</span>
-                <div>
-                    <button>編集</button>
-                    <button>削除</button>
+                <div className="flex gap-2">
+                    <button className="text-gray-700">編集</button>
+                    <button className="text-red-400">削除</button>
                 </div>
             </li>
         </ul>
