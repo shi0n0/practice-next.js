af1efd531bc9cca2fe7ba2a6fb9ddbb3c8956345
shi0n0 <shiono.yui1080@gmail.com>
2023-08-26 22:57:02 +0900

    タスク追加のロジックを作成

--- src/app/components/submit.tsx
+++ src/app/components/submit.tsx
@@ -1,8 +1,21 @@
+"use client"
+
+import { use, useState } from "react";
+
+
 export default function Submit() {
+    const [todos,setTodos] = useState<string[]>([]);
+    const [task,setTask] = useState<string>("");
+
+    const addTodos = () => {
+        setTodos([...todos, task]);
+        setTask("");
+      };
+
     return(
         <form action="" className="flex flex-col">
-            <input type="text" className="p-2 m-2 border rounded focus:outline-none" />
-            <button className="text-white bg-blue-500 p-2 m-2 transform hover:bg-blue-400 hover:scale-95 duration-100">タスク追加</button>
+            <input type="text" onChange={(e) => setTask(e.target.value)} className="p-2 border rounded my-2 focus:outline-none" />
+            <button onClick={addTodos} className="text-white bg-blue-500 p-2 transform my-2 hover:bg-blue-400 hover:scale-95 duration-100">タスク追加</button>
         </form>
     );
 }
