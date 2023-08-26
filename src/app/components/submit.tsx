b7c4201956025840b242ca507b5af91a5261c114
shi0n0 <shiono.yui1080@gmail.com>
2023-08-26 23:22:00 +0900

    単一責任の原則に基づいて修正,型情報を追加

--- src/app/components/submit.tsx
+++ src/app/components/submit.tsx
@@ -2,20 +2,22 @@
 
 import { use, useState } from "react";
 
+interface SubmitProps {
+    onAddTask: (task: string) => void; // onAddTask の型情報を指定
+  }
 
-export default function Submit() {
-    const [todos,setTodos] = useState<string[]>([]);
+export default function Submit({onAddTask}: SubmitProps) {
     const [task,setTask] = useState<string>("");
 
-    const addTodos = () => {
-        setTodos([...todos, task]);
+    const addTask = () => {
+        onAddTask(task)
         setTask("");
       };
 
     return(
         <form action="" className="flex flex-col">
             <input type="text" onChange={(e) => setTask(e.target.value)} value={task} className="p-2 border rounded my-2 focus:outline-none" />
-            <button onClick={addTodos} className="text-white bg-blue-500 p-2 transform my-2 hover:bg-blue-400 hover:scale-95 duration-100">タスク追加</button>
+            <button onClick={addTask} className="text-white bg-blue-500 p-2 transform my-2 hover:bg-blue-400 hover:scale-95 duration-100">タスク追加</button>
         </form>
     );
 }
