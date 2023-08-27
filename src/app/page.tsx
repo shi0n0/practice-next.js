5f461f84e9555d58f57093f388707c3955e3cbb3
shi0n0 <shiono.yui1080@gmail.com>
2023-08-28 00:56:26 +0900

    fix a typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
+import { getAllTodos } from '@/api';
 import Submit from './components/submit';
 import Task from './components/task';
 
-export default function Page() {
+export default async function Page() {
+  const todos = await getAllTodos()
+  console.log(todos);
+
   return (
       <div>
         <h1 className='text-xl font-bold text-gray-700 p-2'>Todo List</h1>
-        <div className='bg-white rounded-lg p-2'>
-          <Submit />
-          <Task />
+        <div className='bg-white rounded-lg p-4'>
+            <Submit />
+          <div className='my-2'>
+            <Task />
+          </div>
         </div>
       </div>
   )
