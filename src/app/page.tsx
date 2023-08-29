a45c62d7fc4706d1ae8d6d6c89a4ada46c5e9d91
shi0n0 <shiono.yui1080@gmail.com>
2023-08-29 23:50:08 +0900

    Taskにtodosを渡す

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
 import { getAllTodos } from '@/api';
 import Submit from './components/submit';
-import Task from './components/task';
+import Task from './components/todolist';
 
 export default async function Page() {
   const todos = await getAllTodos()
-  console.log(todos);
 
   return (
       <div>
         <h1 className='text-xl font-bold text-gray-700 p-2'>Todo List</h1>
         <div className='bg-white rounded-lg p-4'>
-            <Submit />
+            <Submit /> 
           <div className='my-2'>
-            <Task />
+            <Task todos={todos} />
           </div>
         </div>
       </div>
