b3e82f00de617c724da89715698028547fb0167b
shi0n0 <shiono.yui1080@gmail.com>
2023-08-30 22:37:25 +0900

    タスクの登録機能(id指定)を追加

--- src/app/components/submit.tsx
+++ src/app/components/submit.tsx
@@ -1,23 +1,26 @@
 "use client"
 
-import { use, useState } from "react";
+import { addTodo } from "@/api";
+import { ChangeEvent, FormEvent, use, useState } from "react";
 
-interface SubmitProps {
-    onAddTask: (task: string) => void; // onAddTask の型情報を指定
-  }
+export default function Submit() {
 
-export default function Submit({onAddTask}: SubmitProps) {
-    const [task,setTask] = useState<string>("");
+    const [taskTitle, setTaskTitle] = useState("")
 
-    const addTask = () => {
-        onAddTask(task)
-        setTask("");
-      };
+    const handleSubmit = async(e: FormEvent) => {
+        e.preventDefault
+
+        await addTodo({ id:"4", text: taskTitle})
+
+    setTaskTitle("")
+    }
 
     return(
-        <form action="" className="flex flex-col">
-            <input type="text" onChange={(e) => setTask(e.target.value)} value={task} className="p-2 border rounded my-2 focus:outline-none" />
-            <button onClick={addTask} className="text-white bg-blue-500 p-2 transform my-2 hover:bg-blue-400 hover:scale-95 duration-100">タスク追加</button>
+        <form action="" className="flex flex-col" onSubmit={handleSubmit}>
+            <input type="text" className="p-2 border rounded my-2 focus:outline-none" onChange={(e: ChangeEvent<HTMLInputElement>) => setTaskTitle(e.target.value)} value={taskTitle} />
+            <button className="text-white bg-blue-500 p-2 transform my-2 hover:bg-blue-400 hover:scale-95 duration-100">
+                タスク追加
+            </button>
         </form>
     );
 }
