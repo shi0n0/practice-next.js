478473a4d5a2c213255f52b0271afd141025976a
shi0n0 <shiono.yui1080@gmail.com>
2023-08-30 23:40:00 +0900

    状態管理と編集機能の追加

--- src/app/components/todo.tsx
+++ src/app/components/todo.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { editTodo } from "@/api";
 import { Task } from "@/type"
 import { Input } from "postcss";
 import { useState } from "react";
@@ -10,16 +11,22 @@ interface TodoProps {
 
 export default function Todo({ todo }: TodoProps) {
 const [isEditing, setIsEditing] = useState(false)
+const [editedTaskTitle, setEditTaskTitle] =useState(todo.text)
 
     const handleEdit = async () => {
         setIsEditing(true)
     }
 
+    const handleSave = async () => {
+        await editTodo(todo.id, editedTaskTitle)
+        setIsEditing(false)
+    }
+
     return (
         <li key={ todo.id } className="flex justify-between p-3">
-        { isEditing ? (<input type="text" />) : (<span>{todo.text}</span>) }
+        { isEditing ? (<input type="text" className="p-1 border border-gray-400 rounded focus:outline-none" value={ editedTaskTitle } onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEditTaskTitle(e.target.value)} />) : (<span>{todo.text}</span>) }
         <div className="flex gap-2">
-            <button className="text-gray-700" onClick={handleEdit}>編集</button>
+            { isEditing ? (<button className="text-gray-700" onClick={handleSave}>登録</button>) : (<button className="text-gray-700" onClick={handleEdit}>編集</button>)}
             <button className="text-red-400">削除</button>
         </div>
     </li>
