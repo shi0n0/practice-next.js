764767a3a3f72cc2b453f4da80996cca98ae61d3
shi0n0 <shiono.yui1080@gmail.com>
2023-08-30 23:01:12 +0900

    編集の状態管理とハンドラーを追加

--- src/app/components/todo.tsx
+++ src/app/components/todo.tsx
@@ -1,15 +1,25 @@
+"use client"
+
 import { Task } from "@/type"
+import { Input } from "postcss";
+import { useState } from "react";
 
 interface TodoProps {
     todo: Task;
 }
 
 export default function Todo({ todo }: TodoProps) {
+const [isEditing, setIsEditing] = useState(false)
+
+    const handleEdit = async () => {
+        setIsEditing(true)
+    }
+
     return (
         <li key={ todo.id } className="flex justify-between p-3">
-        <span>{todo.text}</span>
+        { isEditing ? (<input type="text" />) : (<span>{todo.text}</span>) }
         <div className="flex gap-2">
-            <button className="text-gray-700">編集</button>
+            <button className="text-gray-700" onClick={handleEdit}>編集</button>
             <button className="text-red-400">削除</button>
         </div>
     </li>
