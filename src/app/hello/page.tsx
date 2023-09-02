458438ebc3668b2ce5024a9403b953e776fe0414
shi0n0 <shiono.yui1080@gmail.com>
2023-09-02 22:36:51 +0900

    必要がなくなったため削除

--- src/app/hello/page.tsx
+++
@@ -1,6 +0,0 @@
-import { error } from "console";
-
-export default async function Page() {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    return <div>this is hello page!</div>
-}
