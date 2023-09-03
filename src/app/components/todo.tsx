d3219e3290bc82aa6c8cda636fea113924d26186
shi0n0 <shiono.yui1080@gmail.com>
2023-09-03 23:55:28 +0900

    レスポンシブの一部対応とコンポーネントのズレの解消

--- src/app/components/todo.tsx
+++ src/app/components/todo.tsx
@@ -3,6 +3,7 @@
 import { editTodo,deleteTodo } from "@/api";
 import { Task } from "@/type"
 import { useState } from "react";
+import ImageComponent from "./image";
 
 interface TodoProps {
     todo: Task;
@@ -26,12 +27,18 @@ const [editedTaskTitle, setEditTaskTitle] =useState(todo.text)
     } 
 
     return (
-        <li key={ todo.id } className="flex justify-between p-3">
-        { isEditing ? (<input type="text" className="p-1 border border-gray-400 rounded focus:outline-none" value={ editedTaskTitle } onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEditTaskTitle(e.target.value)} />) : (<span>{todo.text}</span>) }
-        <div className="flex gap-2">
-            { isEditing ? (<button className="text-gray-700 py-1 px-2 rounded-lg hover:bg-gray-200 duration-300" onClick={handleSave}>登録</button>) : (<button className="text-gray-700 py-1 px-2 rounded-lg hover:bg-gray-200 duration-300" onClick={handleEdit}>編集</button>)}
-            <button className="text-red-400 py-1 px-2 rounded-lg hover:bg-gray-200 duration-300" onClick={handleDelete}>削除</button>
-        </div>
+        <li key={ todo.id } className="flex justify-between p-3 w-full h-full">
+        { isEditing ? (<input type="text" className="p-1 border border-gray-400 rounded focus:outline-none" value={ editedTaskTitle } onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEditTaskTitle(e.target.value)} />) : (
+        <div className="w-full h-96">
+            <div className="flex justify-between">
+                <span>{todo.text}</span>
+                <div className="flex gap-2">
+                { isEditing ? (<button className="text-gray-700 py-1 px-2 rounded-lg hover:bg-gray-200 duration-300" onClick={handleSave}>登録</button>) : (<button className="text-gray-700 py-1 px-2 rounded-lg hover:bg-gray-200 duration-300" onClick={handleEdit}>編集</button>)}
+                <button className="text-red-400 py-1 px-2 rounded-lg hover:bg-gray-200 duration-300" onClick={handleDelete}>削除</button>
+                </div>
+            </div>
+            <ImageComponent />
+        </div>) }
     </li>
     )
 } 
