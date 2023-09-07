fe73d674109a0a85063f7744feef12ca38268d22
shi0n0 <shiono.yui1080@gmail.com>
2023-09-07 17:57:31 +0900

    ローカルのの機能を削除したのち、データベースと連携した削除機能をつけた

--- src/app/components/todo.tsx
+++ src/app/components/todo.tsx
@@ -1,54 +1,44 @@
-"use client"
-
-import { editTodo,deleteTodo } from "@/api";
-import { Task } from "@/type"
+import { deleteTodo } from "@/api";
 import { useState } from "react";
-import ImageComponent from "./image";
+import { Tasks } from "@prisma/client";
+import { redirect } from "next/navigation";
 
-interface TodoProps {
-    todo: Task;
-}
 
-export default function Todo({ todo }: TodoProps) {
-const [isEditing, setIsEditing] = useState(false)
-const [editedTaskTitle, setEditTaskTitle] =useState(todo.text)
-
-    const handleEdit = async () => {
-        setIsEditing(true)
+const Todo = ({ task }: { task: Tasks }) => {
+  const handleDelete = async () => {
+    "use server";
+    try {
+      await deleteTodo(task.id);
+      redirect("/");
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    const handleSave = async () => {
-        await editTodo(todo.id, editedTaskTitle)
-        setIsEditing(false)
-    }
- 
-    const handleDelete = async () => {
-        await deleteTodo(todo.id);
-    } 
-
-    return (
-        <li key={ todo.id } className="w-full h-full mb-2 bg-blue-100 rounded-t-lg border">
-        { isEditing ? (
-        <div className="w-full flex justify-between bg-blue-50 p-3 rounded-t-lg">
-            <input type="text" className="border border-gray-400 rounded focus:outline-none" value={ editedTaskTitle } onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEditTaskTitle(e.target.value)} />
-            <div className="gap-2">
-                <button className="text-gray-700 py-1 px-2 rounded-lg hover:bg-gray-200 duration-300" onClick={handleSave}>登録</button>
-                <button className="text-red-400 py-1 px-2 rounded-lg hover:bg-gray-200 duration-300" onClick={handleDelete}>削除</button>
-            </div>
+  return (
+    <li
+      key={task.id}
+      className="w-full h-full mb-2 bg-blue-100 rounded-t-lg border"
+    >
+      <div className="w-full flex justify-between bg-blue-50 p-3 rounded-t-lg">
+        <span>{task.text}</span>
+        <div className="gap-2">
+          <button className="text-gray-700 py-1 px-2 rounded-lg hover:bg-gray-200 duration-300">
+            編集
+          </button>
+          <form>
+            <button
+              type="submit"
+              className="text-red-400 py-1 px-2 rounded-lg hover:bg-gray-200 duration-300"
+              formAction={handleDelete}
+            >
+              削除
+            </button>
+          </form>
         </div>
-        ): 
-        (
-        <div className="w-full flex justify-between bg-blue-50 p-3 rounded-t-lg">
-            <span>{todo.text}</span>
-            <div className="gap-2">
-                <button className="text-gray-700 py-1 px-2 rounded-lg hover:bg-gray-200 duration-300" onClick={handleEdit}>編集</button>
-                <button className="text-red-400 py-1 px-2 rounded-lg hover:bg-gray-200 duration-300" onClick={handleDelete}>削除</button>
-            </div>
-        </div>
-        ) }
-        <div className="w-full">
-            <ImageComponent />
-        </div>
+      </div>
     </li>
-    )
-} 
+  );
+};
+
+export default Todo;
