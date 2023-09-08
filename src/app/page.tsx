cc3c24438793bdbab018414d411372cb57633233
shi0n0 <shiono.yui1080@gmail.com>
2023-09-08 23:51:25 +0900

    CSSの調整とinput,buttonタグの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,28 @@ const Page = async() => {
       <div>
         <h1 className='text-xl font-bold text-gray-700 p-2'>Todo List</h1>
         <div className='bg-white rounded-lg p-4'>
-          <div className='my-2'>
-          <ul className="mt-8">
+          <ul>
             {tasks.map((task) => (
-              <li key={task.id}>{task.text}</li>
+              <li className='p-1' key={task.id}>{task.text}</li>
             ))}
           </ul>
-          </div>
+          <form action="">
+            <div className='mt-3'>
+              <input
+              className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500"
+              type="text"
+              placeholder="新しいタスクを入力"
+              />
+              <button
+                className="w-full mt-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 rounded-md transition duration-300 ease-in-out focus:outline-none"
+              >
+                タスクを追加
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-  )
-}
+    )
+  }
 
 export default Page;
