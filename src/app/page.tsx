36fa624632f2e69d552d25cf2c45caedf32c4930
shi0n0 <shiono.yui1080@gmail.com>
2023-09-09 00:12:35 +0900

    addTodo関数の有効化(エラーの解消)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,11 @@ import prisma from '../../prisma/prisma';
 const Page = async() => {
   const tasks = await prisma.tasks.findMany();
 
+  const addTodo = async() => {
+    "use server"
+     console.log('タスク追加ボタンが押されました');
+  };
+
   return (
       <div>
         <h1 className='text-xl font-bold text-gray-700 p-2'>Todo List</h1>
@@ -12,7 +17,7 @@ const Page = async() => {
               <li className='p-1' key={task.id}>{task.text}</li>
             ))}
           </ul>
-          <form action="">
+          <form action={addTodo}>
             <div className='mt-3'>
               <input
               className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500"
@@ -20,7 +25,8 @@ const Page = async() => {
               placeholder="新しいタスクを入力"
               />
               <button
-                className="w-full mt-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 rounded-md transition duration-300 ease-in-out focus:outline-none"
+                type='submit'
+                className="w-full mt-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 rounded-md transition duration-100 ease-in-out focus:outline-none"
               >
                 タスクを追加
               </button>
