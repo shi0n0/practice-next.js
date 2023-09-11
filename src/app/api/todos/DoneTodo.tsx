3f6472c9e191dc8c800e3a980636055f770a6fb2
shi0n0 <shiono.yui1080@gmail.com>
2023-09-11 17:22:55 +0900

    チェックボックスのデザインの追加

--- src/app/api/todos/DoneTodo.tsx
+++ src/app/api/todos/DoneTodo.tsx
@@ -12,11 +12,14 @@ const DoneTodo = ({
   let [isPending, startTransition] = useTransition();
 
   return (
-    <input
-      onChange={() => startTransition(() => doneTodo(id, isCompleted))}
-      checked={isCompleted}
-      type="checkbox"
-    />
+    <label className="flex items-center space-x-2 cursor-pointer">
+      <input
+        onChange={() => startTransition(() => doneTodo(id, isCompleted))}
+        checked={isCompleted}
+        type="checkbox"
+        className="h-5 w-5 text-blue-600 border border-gray-300 rounded-md checked:bg-blue-600 checked:border-transparent focus:ring focus:ring-blue-300 transition-colors"
+      />
+    </label>
   );
 };
 
